c7d486e74f009d6d03911a87afe5a3a71c8264d6
MaksimSmolenkov <makssmolenkovmail.ru@gmail.com>
2024-10-12 10:33:03 +0600

    commit2

--- tests/additem.js
+++ tests/additem.js
@@ -4,25 +4,23 @@ const LoginPage = require('../pages/LoginPage');
 const ProductsPage = require('../pages/ProductsPage');
 const CartPage = require('../pages/CartPage');
 
-test('test', async () => {
+test('Login and add product to cart', async () => {
     browser = await chromium.launch();
     page = await browser.newPage();
     loginPage = new LoginPage(page);
     productsPage = new ProductsPage(page);
-    cartPage = new CartPage(page);;
+    cartPage = new CartPage(page);
     
     await page.goto('https://www.saucedemo.com/');
     
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password', 'secret_sauce');
-    
-    await page.click('#login-button');
+    await loginPage.login('standard_user', 'secret_sauce');
     
     // Добавляем первый товар в корзину
-    await page.click('.btn_inventory'); // Кнопка "Добавить в корзину" для первого товара
+    await productsPage.addProductToCart(1); // Кнопка "Добавить в корзину" для первого товара
     
     // Проверяем, что товар добавлен в корзину
-    const cartCount = await page.locator('.shopping_cart_badge').innerText();
+    const cartCount = await cartPage.getCartCount();
+    expect(cartCount).toBe('1');
 
     await browser.close();
 })();
--- tests/wronguser.js
+++ tests/wronguser.js
@@ -4,21 +4,19 @@ const LoginPage = require('../pages/LoginPage');
 const ProductsPage = require('../pages/ProductsPage');
 const CartPage = require('../pages/CartPage');
 
-test('test1', async () => {
+test('Login with wrong credentials', async () => {
     browser = await chromium.launch();
+    page = await browser.newPage();
     loginPage = new LoginPage(page);
     productsPage = new ProductsPage(page);
     cartPage = new CartPage(page);
-    
     await page.goto('https://www.saucedemo.com/');
     
-    await LoginPage.fill('#user-name', 'wrong_user');
-    await page.fill('#password', 'wrong_password');
+    await loginPage.login('wrong_user', 'wrong_password');
     
-    await page.click('#login-button');
-    
-    // Проверяем наличие сообщения об ошибке
-    const errorMessage = await page.locator('.Epic sadface: Username and password do not match any user in this service.').innerText();
+    const errorMessage = await loginPage.getErrorMessage();
+    expect(errorMessage).toContain('.Epic sadface: Username and password do not match any user in this service.');
+
 
     await browser.close();
 })();
